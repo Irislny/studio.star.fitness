d18812e959a342f3df4edffd5f6703b452fa752e
Irislny <i.lannoy@ileps.fr>
2025-11-03 10:30:15 +0100

    Add simple contact button functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+// Gestion simple d'un bouton d'action (popup d'exemple)
+// Tu peux le remplacer par un vrai formulaire de contact, redirection, etc.
+
+document.getElementById('contactBtn').addEventListener('click', function () {
+    alert("Prenez contact avec nous pour plus d'informations ou pour réserver votre premier cours !");
+});
